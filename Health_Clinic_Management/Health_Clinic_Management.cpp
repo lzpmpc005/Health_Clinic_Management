f1910802a2671323419d8df1c8d4a335e0912eec
Charton <charton0913@gmail.com>
2023-12-03 10:37:49 +0100

    appointment function added

--- Health_Clinic_Management/Health_Clinic_Management.cpp
+++ Health_Clinic_Management/Health_Clinic_Management.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 #include<vector>
 #include<fstream>
-#include <iostream>
 #include <cstdlib>
 #include <ctime>
 
@@ -10,23 +9,40 @@ using namespace std;
 // Structure the data of Patient 
 class Patient {
 public:
-    int P_id;
+    int P_id{};
     string P_name;
     string P_address;
     string P_history;
 
     friend ostream& operator<<(ostream & out, Patient & p) {
-        out << p.P_id << " , " << p.P_name << " , " << p.P_address << " , " << p.P_history << "\n";
+        out << p.P_id << "," << p.P_name << "," << p.P_address << "," << p.P_history << "\n";
 
-        return out;}
-    
-    friend istream& operator>>(istream & in, Patient & p) {
-        in >> p.P_name >> p.P_address >> p.P_history;
-
-    return in; 
+        return out;
     }
+
+    friend istream& operator>>(istream& in, Patient& p) {
+        char comma; // to read the comma
+        in >> p.P_id >> comma >> p.P_name >> comma >> p.P_address >> comma >> p.P_history;
+
+        return in;
+    }
+
 };
 
+// Structure the data of Appointment
+class Appointment {
+public:
+    int A_id{};
+    int P_id{};
+    string P_name;
+    string Comments;
+
+    friend ostream& operator<<(ostream & out, Appointment & a) {
+        out << a.A_id << "," << a.P_id << "," << a.P_name << "," << a.Comments << "\n";
+
+        return out;
+    }
+};
 
 void PatientRegister()
 {
@@ -59,12 +75,78 @@ void PatientRegister()
     cout << "\nPatient Registered successfully\n" << "Patient ID is : "<<p.P_id<<"\n\n";
     }
 
-void MakeAppointment() {
-    
+// Read data from patient.txt
+vector<Patient> ReadPatientsFromFile() {
+    vector<Patient> patients;
+    ifstream infile("patients.txt");
+    Patient p;
+
+    if (infile.is_open()) {
+        while (infile >> p) {
+            patients.push_back(p);
+        }
+        infile.close();
+    } else {
+        cout << "Unable to open file.\n";
+    }
+
+    return patients;
+}
+
+// Make an appointment by Patient ID, current seats <= 10
+int current_seats = 10 ;
+
+void MakeAppointment(vector<Patient>& patients)
+{
+    Appointment a;
+    Patient p;
+// Check current seats
+
+    // Check if Patient ID exists
+    int PatientID;
+    cout << "Enter Patient ID for appointment: ";
+    cin >> PatientID;
+
+    bool PatientExists = false;
+    for (const Patient& p : patients) {
+        if (p.P_id == PatientID) {
+            PatientExists = true;
+            a.P_id = p.P_id;
+            a.P_name = p.P_name;
+            break;
+        }
+    }
+
+    // If Patient Exist Make Appointment Else Return Error
+    if (PatientExists) {
+        //Write appointment logs into file
+        ofstream outfile("appointments.txt", ios::app);
+        if (outfile.is_open()) {
+            srand(time(NULL)); // generate a unique appointment ID
+            a.A_id = rand() % 9999999 + 1000000;
+
+            cout << "Additional Comments: ";
+            cin >> a.Comments;
+
+            outfile << a;
+            outfile.close();
+
+            cout << "\nAppointment made successfully\n" << "Appointment ID is : "<<a.A_id<<"\n\n";
+
+        }
+        else {
+            cout << "Unable to open file.\n";
+        }
+    }
+    else{
+        cout << "Patient ID not found. Please register first.\n";
+    }
 }
 
 int main()
 {
+    vector<Patient> patients = ReadPatientsFromFile();
+
 m:
 	cout << "\n\t\t\t Select Function: ";
 	cout << "\n\n1) REGISTER";
@@ -80,7 +162,7 @@ m:
         PatientRegister();
 		break;
 	case 2:
-        MakeAppointment();
+        MakeAppointment(patients);
 		break;
 	case 3:
 		exit(0);
